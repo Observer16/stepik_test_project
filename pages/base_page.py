0bbb6f3ef845795a1d9eb2a7c58a0bc48922f4e9
Виталий Алтунин <vitalijaltunin@MacBook-Air-Vitalij.local>
2022-01-26 12:04:27 +0300

    Добавлены изменения в файлы

---
+++ pages/base_page.py
@@ -0,0 +1,15 @@
+class BasePage(object):
+
+    def __init__(self, browser, url):
+        """Конструктор класса.
+        :параметр browser:
+        :параметр url:
+        """
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        """метод открывает нужную страницу,
+        используя метод get()
+        """
+        self.browser.get(self.url)
